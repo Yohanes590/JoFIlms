9a5ae9e002c325ae42d1a88fd0cdaef1a47bb82a
Yohanes590 <jplussince34@gmail.com>
2025-02-26 11:26:16 -0900

    PROJECT

--- frontend/src/scripts/apiKey.js
+++ frontend/src/scripts/apiKey.js
@@ -1,2 +1,2 @@
 export const APIKEY = "8729ecb77d4cbac548274a121132ffe3"
-export const ConstantLink = "http://localhost:3000"
+export const ConstantLink = "https://jo-films-api-server-req.onrender.com"
